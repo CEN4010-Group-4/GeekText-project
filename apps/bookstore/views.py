6398c358d190f4eaa196c9b79d346d8bb28b9c90
adie9 <70285960+adie9@users.noreply.github.com>
2021-03-15 22:56:42 -0400

    Added pagination

--- apps/bookstore/views.py
+++ apps/bookstore/views.py
@@ -1,21 +1,30 @@
 from django.shortcuts import render
 from django.db.models import Q
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
 from .models import Product
 from .models import Author
+from .models import Genre
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
-# Create your views here.
-class SearchView(TemplateView):
+
+class SearchView(ListView):
     template_name = "search.html"
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
+    context_object_name = "products"
+    def get_queryset(self):
+        queryset_list = Product.objects.all()
         kw = self.request.GET.get("q")
-        results = Product.objects.filter(Q(name__icontains=kw) | Q(genre__name__icontains=kw) | Q(author__first_name__icontains=kw) | Q(author__last_name__icontains=kw))
-        
-        context["results"] = results
-        return context
+        if Q:
+            queryset_list = queryset_list.filter(Q(name__icontains=kw) | Q(genre__name__icontains=kw) | Q(author__first_name__icontains=kw) | Q(author__last_name__icontains=kw))
+        paginator = Paginator(queryset_list, 10)
+        page = self.request.GET.get('page')
+        try:
+            queryset_list = paginator.page(page)
+        except PageNotAnInteger:
+            queryset_list = paginator.page(1)
+        except EmptyPage:
+            queryset_list = paginator.page(paginator.num_pages)
+        return queryset_list
 
 def book_details(request):
     obj = Product.objects.get(id=1)
@@ -52,4 +61,5 @@ def all_books(request):
 
 
 def books(request):
+    
     return render(request,'bookstore/books.html')
