34cba7d18e43c2e783ef133668cbfa6f1dcbfdd4
sharb010 <77760048+sharb010@users.noreply.github.com>
2021-03-24 19:47:40 -0400

    change to settings.py for static media

--- GeekText/settings.py
+++ GeekText/settings.py
@@ -132,5 +132,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-
+MEDIA_URL = '/media/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 CRISPY_TEMPLATE_PACK='bootstrap4'
